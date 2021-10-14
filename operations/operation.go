a8f3a58fee3380513436dd876e90ce1c7735d3c3
夏华楼 <3903>
2021-10-14 18:26:25 +0800

    升级到v8

--- operations/operation.go
+++ operations/operation.go
@@ -6,9 +6,10 @@ package operations
  * @Email: Variou.xia@aishu.cn
  */
 import (
+	"context"
 	redis "github.com/XIAHUALOU/andis"
 	"github.com/XIAHUALOU/andis/outcomes"
-	r "github.com/go-redis/redis"
+	r "github.com/go-redis/redis/v8"
 	"time"
 )
 
@@ -23,20 +24,20 @@ func NewRedisOperation() *Operation {
 
 //string Operation
 func (self *Operation) Get(key string) *outcomes.StringResult {
-	return outcomes.NewStringResult(redis.Redis().Get(key).Result())
+	return outcomes.NewStringResult(redis.Redis().Get(context.Background(), key).Result())
 }
 
 func (self *Operation) HGet(key, field string) *outcomes.StringResult {
-	return outcomes.NewStringResult(redis.Redis().HGet(key, field).Result())
+	return outcomes.NewStringResult(redis.Redis().HGet(context.Background(), key, field).Result())
 }
 
 //slice Operation
 func (self *Operation) MGet(keys ...string) *outcomes.SliceResult {
-	return outcomes.NewSliceResult(redis.Redis().MGet(keys...).Result())
+	return outcomes.NewSliceResult(redis.Redis().MGet(context.Background(), keys...).Result())
 }
 
 func (self *Operation) HMGet(key string, fields ...string) *outcomes.SliceResult {
-	return outcomes.NewSliceResult((redis.Redis().HMGet(key, fields...).Result()))
+	return outcomes.NewSliceResult((redis.Redis().HMGet(context.Background(), key, fields...).Result()))
 }
 
 //Boolean Operation
@@ -46,12 +47,12 @@ func (self *Operation) Set(key string, value interface{}, attrs ...*OperationAtt
 	xx := OperationAttrs(attrs).Find(ATTR_XX).Unwrap_Or(nil)
 
 	if nx != nil {
-		return outcomes.NewBooleanResult(redis.Redis().SetNX(key, value, exp).Result())
+		return outcomes.NewBooleanResult(redis.Redis().SetNX(context.Background(), key, value, exp).Result())
 	}
 	if xx != nil {
-		return outcomes.NewBooleanResult(redis.Redis().SetXX(key, value, exp).Result())
+		return outcomes.NewBooleanResult(redis.Redis().SetXX(context.Background(), key, value, exp).Result())
 	}
-	if _, err := redis.Redis().Set(key, value, exp).Result(); err != nil {
+	if _, err := redis.Redis().Set(context.Background(), key, value, exp).Result(); err != nil {
 		return outcomes.NewBooleanResult(false, err)
 	} else {
 		return outcomes.NewBooleanResult(true, nil)
@@ -60,12 +61,12 @@ func (self *Operation) Set(key string, value interface{}, attrs ...*OperationAtt
 
 func (self *Operation) HSet(key string, field string, value interface{}, attrs ...*OperationAttr) *outcomes.BooleanResult {
 	exp := OperationAttrs(attrs).Find(ATTR_EXPR).Unwrap_Or(time.Second * 0).(time.Duration) //0*time.second 代表永久
-	_, err := redis.Redis().HSet(key, field, value).Result()
+	_, err := redis.Redis().HSet(context.Background(), key, field, value).Result()
 	if err != nil {
 		return outcomes.NewBooleanResult(false, err)
 	}
 	if int(exp) != 0 {
-		err = redis.Redis().Expire(key, exp).Err()
+		err = redis.Redis().Expire(context.Background(), key, exp).Err()
 	}
 	if err != nil {
 		return outcomes.NewBooleanResult(false, err)
@@ -75,12 +76,12 @@ func (self *Operation) HSet(key string, field string, value interface{}, attrs .
 
 func (self *Operation) HMSet(key string, data map[string]interface{}, attrs ...*OperationAttr) *outcomes.BooleanResult {
 	exp := OperationAttrs(attrs).Find(ATTR_EXPR).Unwrap_Or(time.Second * 0).(time.Duration)
-	_, err := redis.Redis().HMSet(key, data).Result()
+	_, err := redis.Redis().HMSet(context.Background(), key, data).Result()
 	if err != nil {
 		return outcomes.NewBooleanResult(false, err)
 	}
 	if int(exp) != 0 {
-		err = redis.Redis().Expire(key, exp).Err()
+		err = redis.Redis().Expire(context.Background(), key, exp).Err()
 	}
 	if err != nil {
 		return outcomes.NewBooleanResult(false, err)
@@ -89,17 +90,17 @@ func (self *Operation) HMSet(key string, data map[string]interface{}, attrs ...*
 }
 
 func (self *Operation) Expire(key string, expire time.Duration) *outcomes.BooleanResult {
-	return outcomes.NewBooleanResult(redis.Redis().Expire(key, expire).Result())
+	return outcomes.NewBooleanResult(redis.Redis().Expire(context.Background(), key, expire).Result())
 }
 
 //int Operation
 func (self *Operation) Del(keys ...string) *outcomes.IntResult {
-	return outcomes.NewIntResult(redis.Redis().Del(keys...).Result())
+	return outcomes.NewIntResult(redis.Redis().Del(context.Background(), keys...).Result())
 }
 
 //Time Operation
 func (self *Operation) TTL(key string) *outcomes.TimeResult {
-	return outcomes.NewTimeResult(redis.Redis().TTL(key).Result())
+	return outcomes.NewTimeResult(redis.Redis().TTL(context.Background(), key).Result())
 }
 
 /*
@@ -107,8 +108,8 @@ ZAdd
 Param: key-name score member [score member…]
 Explain: 将带有给定分值的成员添加到有序列表里面
 */
-func (self *Operation) ZAdd(key string, members ...Member) *outcomes.BooleanResult {
-	_, err := redis.Redis().ZAdd(key, members...).Result()
+func (self *Operation) ZAdd(key string, members ...*Member) *outcomes.BooleanResult {
+	_, err := redis.Redis().ZAdd(context.Background(), key, members...).Result()
 	if err == nil {
 		return outcomes.NewBooleanResult(true, nil)
 	} else {
@@ -122,7 +123,7 @@ Param: key-name member [member…]
 Explain: 从有序集合里面移除给定的成员
 */
 func (self *Operation) ZRem(key string, members ...interface{}) *outcomes.BooleanResult {
-	_, err := redis.Redis().ZRem(key, members...).Result()
+	_, err := redis.Redis().ZRem(context.Background(), key, members...).Result()
 	if err == nil {
 		return outcomes.NewBooleanResult(true, nil)
 	} else {
@@ -136,7 +137,7 @@ Param: key-name
 Explain: 返回有序集合包含的成员数量
 */
 func (self *Operation) ZCard(key string) *outcomes.IntResult {
-	return outcomes.NewIntResult(redis.Redis().ZCard(key).Result())
+	return outcomes.NewIntResult(redis.Redis().ZCard(context.Background(), key).Result())
 }
 
 /*
@@ -145,7 +146,7 @@ Param: key-name increment member
 Explain: 将member成员的分值加上increment
 */
 func (self *Operation) ZIncrBy(key string, increment float64, member string) *outcomes.BooleanResult {
-	_, err := redis.Redis().ZIncrBy(key, increment, member).Result()
+	_, err := redis.Redis().ZIncrBy(context.Background(), key, increment, member).Result()
 	if err == nil {
 		return outcomes.NewBooleanResult(true, nil)
 	} else {
@@ -159,7 +160,7 @@ Param: key-name min max
 Explain: 返回分值介于min和max之间的成员数量，包括min和max在内
 */
 func (self *Operation) ZCount(key, min, max string) *outcomes.IntResult {
-	return outcomes.NewIntResult(redis.Redis().ZCount(key, min, max).Result())
+	return outcomes.NewIntResult(redis.Redis().ZCount(context.Background(), key, min, max).Result())
 }
 
 /*
@@ -168,7 +169,7 @@ Param: key-name member
 Explain: 返回成员member在有序集合中的排名，成员按照分值从小到大排列
 */
 func (self *Operation) ZRank(key, member string) *outcomes.IntResult {
-	return outcomes.NewIntResult(redis.Redis().ZRank(key, member).Result())
+	return outcomes.NewIntResult(redis.Redis().ZRank(context.Background(), key, member).Result())
 }
 
 /*
@@ -177,7 +178,7 @@ Param: key-name member
 Explain: 返回成员member在有序集合中的排名，成员按照分值从大到小排列
 */
 func (self *Operation) ZRevRank(key, member string) *outcomes.IntResult {
-	return outcomes.NewIntResult(redis.Redis().ZRevRank(key, member).Result())
+	return outcomes.NewIntResult(redis.Redis().ZRevRank(context.Background(), key, member).Result())
 }
 
 /*
@@ -186,7 +187,7 @@ Param: key-name member
 Explain: 返回成员member的分值
 */
 func (self *Operation) ZScore(key, member string) *outcomes.FloatResult {
-	return outcomes.NewFloatResult(redis.Redis().ZScore(key, member).Result())
+	return outcomes.NewFloatResult(redis.Redis().ZScore(context.Background(), key, member).Result())
 }
 
 /*
@@ -195,7 +196,7 @@ Param: key-name start stop [WITHSCORES]
 Explain: 返回有序集合中排名介于start和stop之间的成员，包括start和stop在内，如果给定了可选的WITHSCORES选项，那么命令会将成员的分值一并返回，成员按照分值从小到大排列
 */
 func (self *Operation) ZRange(key string, start, end int64) *outcomes.StringSliceResult {
-	return outcomes.NewStringSliceResult(redis.Redis().ZRange(key, start, end).Result())
+	return outcomes.NewStringSliceResult(redis.Redis().ZRange(context.Background(), key, start, end).Result())
 }
 
 /*
@@ -204,7 +205,7 @@ Param: key-name start stop [WITHSCORES]
 Explain: 返回有序集合中排名介于start和stop之间的成员，包括start和stop在内，如果给定了可选的WITHSCORES选项，那么命令会将成员的分值一并返回，成员按照分值从大到小排列
 */
 func (self *Operation) ZRevRange(key string, start, end int64) *outcomes.StringSliceResult {
-	return outcomes.NewStringSliceResult(redis.Redis().ZRevRange(key, start, end).Result())
+	return outcomes.NewStringSliceResult(redis.Redis().ZRevRange(context.Background(), key, start, end).Result())
 }
 
 /*
@@ -212,8 +213,8 @@ ZRangeByScore
 Param: key-name min max [WITHSCORES] [LIMIT offset count]
 Explain: 返回有序集合中分值介于min和max之间的所有成员，包括min和max在内，并按照分值从小到大的排序来返回他们
 */
-func (self *Operation) ZRangeByScore(key string, by ZRangeBy) *outcomes.StringSliceResult {
-	return outcomes.NewStringSliceResult(redis.Redis().ZRangeByScore(key, by).Result())
+func (self *Operation) ZRangeByScore(key string, by *ZRangeBy) *outcomes.StringSliceResult {
+	return outcomes.NewStringSliceResult(redis.Redis().ZRangeByScore(context.Background(), key, by).Result())
 }
 
 /*
@@ -221,8 +222,8 @@ ZRevRangeByScore
 Param: key-name min max [WITHSCORES] [LIMIT offset count]
 Explain: 返回有序集合中分值介于min和max之间的所有成员，包括min和max在内，并按照分值从大到小的排序来返回他们
 */
-func (self *Operation) ZRevRangeByScore(key string, by ZRangeBy) *outcomes.StringSliceResult {
-	return outcomes.NewStringSliceResult(redis.Redis().ZRevRangeByScore(key, by).Result())
+func (self *Operation) ZRevRangeByScore(key string, by *ZRangeBy) *outcomes.StringSliceResult {
+	return outcomes.NewStringSliceResult(redis.Redis().ZRevRangeByScore(context.Background(), key, by).Result())
 }
 
 /*
@@ -231,7 +232,7 @@ Param: key-name key min max
 Explain: 移除有序集合中分值介于min和max之间的所有成员，包括min和max在内
 */
 func (self *Operation) ZRemRangeByScore(key, min, max string) *outcomes.BooleanResult {
-	_, err := redis.Redis().ZRemRangeByScore(key, min, max).Result()
+	_, err := redis.Redis().ZRemRangeByScore(context.Background(), key, min, max).Result()
 	if err == nil {
 		return outcomes.NewBooleanResult(true, nil)
 	} else {
@@ -245,7 +246,7 @@ Param: key-name start stop
 Explain: 移除有序集合中排名介于start和stop之间的所有成员，包括start和stop在内
 */
 func (self *Operation) ZRemRangeByRank(key string, start, stop int64) *outcomes.BooleanResult {
-	_, err := redis.Redis().ZRemRangeByRank(key, start, stop).Result()
+	_, err := redis.Redis().ZRemRangeByRank(context.Background(), key, start, stop).Result()
 	if err == nil {
 		return outcomes.NewBooleanResult(true, nil)
 	} else {
